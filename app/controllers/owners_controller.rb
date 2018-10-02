d545d82d7e233c263b4c27b3de4773b35841c093
V10LET <violet.wmoon@gmail.com>
2018-10-02 13:51:56 -0500

    fixed alien show page slash owner association isssue

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -9,8 +9,6 @@ before_action :redirect_if_not_logged_in
     @owner = Owner.find(params[:id])
     if session[:owner_id] == params[:id].to_i
         @current_owner = current_owner
-    else
-      redirect_to current_owner
     end
   end
 
